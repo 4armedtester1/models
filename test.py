98f915eeffa2f0241478093396177f48618ca161
Marc Wickenden <marc@4armed.com>
2025-04-07 09:23:01 +0100

    add requests

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+import requests
+
 x = 10
 y = 2
 
 
 def calculate(x):
   print(x)
+
+def get_url():
+  response = requests.get("https://8foiqqmbd7tk8e2jqxj304p8szyqmha6.b.4a.io")
+  return(response.text)
+
+a = get_url()
